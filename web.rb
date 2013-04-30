64d220e9c0567354b21dd7be165a889b6716b0f6
hzmangel <hzmangel@gmail.com>
2013-04-30 16:02:08 +0800

    微信网址接入

--- web.rb
+++ web.rb
@@ -1,9 +1,21 @@
 require 'json'
 require 'sinatra'
 require "sinatra/json"
+require 'digest/sha1'
 
 get '/' do
   puts params
-  content_type :json
-  params.to_json
+
+  sign = params[:signature]
+  echostr = params[:echostr]
+  timestamp = params[:timestamp]
+  nonce = params[:nonce]
+  token = "5vcooking"
+
+  signed_value = Digest::SHA1.hexdigest([token, timestamp, nonce].sort.join)
+
+  if signed_value == sign
+    echostr
+  end
+
 end
